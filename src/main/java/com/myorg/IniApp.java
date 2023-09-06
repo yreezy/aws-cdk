bcc889ca1ad65689186d13c87704aa68cd25ad50
charles <196218030@qq.com>
2023-09-06 18:09:41 +0800

    引入 guava 增加VPC 初始化

--- src/main/java/com/myorg/IniApp.java
+++ src/main/java/com/myorg/IniApp.java
@@ -4,12 +4,10 @@ import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 
-import java.util.Arrays;
-
 public class IniApp {
     public static void main(final String[] args) {
         App app = new App();
-
+        // execute with terminal command: cdk deploy --profile <PROFILE>
         new IniStack(app, "IniStack", StackProps.builder()
                 // If you don't specify 'env', this stack will be environment-agnostic.
                 // Account/Region-dependent features and context lookups will not work,
@@ -18,12 +16,11 @@ public class IniApp {
                 // Uncomment the next block to specialize this stack for the AWS Account
                 // and Region that are implied by the current CLI configuration.
                 /*
-                .env(Environment.builder()
-                        .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
-                        .region(System.getenv("CDK_DEFAULT_REGION"))
-                        .build())
+                // .env(Environment.builder()
+                //         .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
+                //         .region(System.getenv("CDK_DEFAULT_REGION"))
+                //         .build())
                 */
-
                 // Uncomment the next block if you know exactly what Account and Region you
                 // want to deploy the stack to.
                 /*
@@ -34,8 +31,11 @@ public class IniApp {
                 */
 
                 // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+                .env(Environment.builder()
+                        .account("695418977136")
+                        .region("us-east-2")
+                        .build())
                 .build());
-
         app.synth();
     }
 }
--- src/main/java/com/myorg/IniStack.java
+++ src/main/java/com/myorg/IniStack.java
@@ -1,10 +1,16 @@
 package com.myorg;
 
-import software.constructs.Construct;
+import com.google.common.collect.Lists;
+import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.ec2.SubnetConfiguration;
+import software.amazon.awscdk.services.ec2.SubnetType;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.sqs.Queue;
+import software.constructs.Construct;
+
+import java.util.List;
 
 public class IniStack extends Stack {
     public IniStack(final Construct scope, final String id) {
@@ -20,5 +26,20 @@ public class IniStack extends Stack {
         // final Queue queue = Queue.Builder.create(this, "IniQueue")
         //         .visibilityTimeout(Duration.seconds(300))
         //         .build();
+        // final Queue queue = Queue.Builder.create(this, "IniQueue")
+        //         .visibilityTimeout(Duration.seconds(300))
+        //         .build();
+
+        // init VPC
+        List<SubnetConfiguration> subnetConfigurations = Lists.newArrayList();
+        subnetConfigurations.add(SubnetConfiguration.builder()
+                .name("public-subnet")
+                .cidrMask(24)
+                .subnetType(SubnetType.PUBLIC)
+                .build());
+        Vpc.Builder.create(this, "mainVPC")
+                .maxAzs(2)
+                .subnetConfiguration(subnetConfigurations)
+                .build();
     }
 }
